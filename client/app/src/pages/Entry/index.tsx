518ae22a42b20e2fb9606e4636d389a3c769e042
이창준 <2018110021@dongguk.edu>
2024-08-14 03:25:50 +0900

    feat: Color System, Directory Setting

---
+++ client/app/src/pages/Entry/index.tsx
@@ -0,0 +1,8 @@
+import * as Styled from "./style";
+
+export default function Entry() {
+    return (
+        <Styled.Container>Entry Page
+        </Styled.Container>
+    );
+}
---
+++ client/app/src/shared/global.ts
@@ -0,0 +1,16 @@
+import {createGlobalStyle} from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+    ${reset}
+    /* 전역 스타일 작성 */
+    @font-face {
+        //font-family: 'SUIT';
+        //src: url('../assets/fonts/SUIT-Regular.otf') format('oft');
+        //font-weight: 500;
+        //font-style: normal;
+    }
+    body {
+        //font-family: 'SUIT', sans-serif;
+    }
+`;
