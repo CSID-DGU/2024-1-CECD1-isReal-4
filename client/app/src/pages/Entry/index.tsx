327706cced4ff51bfcf3470dd5118d81004bd508
이창준 <2018110021@dongguk.edu>
2024-08-14 03:39:58 +0900

    feat: FontSystem, Font Component

--- client/app/src/pages/Entry/index.tsx
+++ client/app/src/pages/Entry/index.tsx
@@ -2,7 +2,7 @@ import * as Styled from "./style";
 
 export default function Entry() {
     return (
-        <Styled.Container>Entry Page
+        <Styled.Container>Entry Page입니다. 편하게 폰트를 확인해주세요. 1233456789!@#$%^&*()_+
         </Styled.Container>
     );
 }
