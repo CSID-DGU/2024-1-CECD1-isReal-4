b7aaaa33eb39e96aacdb0a44ee6e39af4efb3ace
Changjoon <127469340+CWDll@users.noreply.github.com>
2024-08-14 15:26:02 +0900

    ✨Feature - ColorSystem, FontSystem, Directory Setting, Font Components (#12)
    * feat: install Styled-components
    
    * feat: Color System, Directory Setting
    
    * feat: FontSystem, Font Component
    
    * feat: add ColorSystem

---
+++ client/app/src/pages/Entry/index.tsx
@@ -0,0 +1,8 @@
+import * as Styled from "./style";
+
+export default function Entry() {
+    return (
+        <Styled.Container>Entry Page입니다. 편하게 폰트를 확인해주세요. 1233456789!@#$%^&*()_+
+        </Styled.Container>
+    );
+}
