9e0e7d2214bbdc168b8ddb58fdeb4c90fe2b3b8c
이창준 <2018110021@dongguk.edu>
2024-08-17 22:40:00 +0900

    feat: AptTitle Component

--- client/app/src/components/Home/AptTitle/index.tsx
+++ client/app/src/components/Home/AptTitle/index.tsx
@@ -1,7 +1,12 @@
-import SizedBox from "@/components/Common/SizedBox";
+import * as Styled from "./style.ts"
+import H2 from "@/components/Common/Font/Heading/H2";
 
 export default function AptTitle() {
+    let aptName = "사용자가 현재 등록한 아파트 이름";
+
     return (
-        <SizedBox width="327px" height="40px"><div>사용자가 현재 등록한 아파트 이름</div></SizedBox>
+        <Styled.AptTitleContainer>
+            <H2 text={aptName}/>
+        </Styled.AptTitleContainer>
     );
 }
