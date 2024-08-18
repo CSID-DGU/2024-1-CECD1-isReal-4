06ae4b0571c87a96ad6f1283b769781ecce37f81
Gu-nuu <gun0005@naver.com>
2024-08-18 18:23:54 +0900

    feat: Login View Router Setting

--- client/app/src/Router.tsx
+++ client/app/src/Router.tsx
@@ -1,16 +1,18 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Entry from "@/pages/Entry";
 import Home from "@/pages/Home";
 import Register from "@/pages/Register";
 import MyPage from "@/pages/MyPage";
+import Login from "@/pages/Login";
 
 export default function Router() {
     return (
         <Routes>
-            <Route path="/" element={<Entry/>}/>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/register" element={<Register/>}/>
-            <Route path="/myPage" element={<MyPage/>}/>
+            <Route path='/' element={<Entry />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/myPage' element={<MyPage />} />
         </Routes>
-    )
+    );
 }
