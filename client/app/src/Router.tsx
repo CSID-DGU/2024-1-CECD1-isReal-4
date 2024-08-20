8714d30370d10da6185115dedfec7f1b92c22f47
Gu-nuu <gun0005@naver.com>
2024-08-20 09:47:20 +0900

    feat: Router Settings

--- client/app/src/Router.tsx
+++ client/app/src/Router.tsx
@@ -4,6 +4,8 @@ import Home from "@/pages/Home";
 import Register from "@/pages/Register";
 import MyPage from "@/pages/MyPage";
 import LogIn from "@/pages/LogIn";
+import SignUp from "@/pages/SignUp";
+import UploadDoc from "./pages/UploadDoc";
 
 export default function Router() {
     return (
@@ -11,6 +13,9 @@ export default function Router() {
             <Route path='/' element={<Entry />} />
             <Route path='/home' element={<Home />} />
             <Route path='/login' element={<LogIn />} />
+            <Route path='/signup' element={<SignUp />}>
+                <Route path='uploadDoc' element={<UploadDoc />} />
+            </Route>
             <Route path='/register' element={<Register />} />
             <Route path='/myPage' element={<MyPage />} />
         </Routes>
