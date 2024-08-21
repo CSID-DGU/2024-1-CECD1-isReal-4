0ffd7a819e52f6e1b17009e3c11dfcc1ac24d816
KiminSon <kimin0331@naver.com>
2024-08-21 21:06:11 +0900

    feat: Spring project initial commit

---
+++ server/apartodo/src/main/java/com/isreal/apartodo/ApartodoApplication.java
@@ -0,0 +1,13 @@
+package com.isreal.apartodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApartodoApplication.class, args);
+    }
+
+}
