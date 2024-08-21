fc5f823d2fe81023e0961bb5a0d9548206447635
이창준 <2018110021@dongguk.edu>
2024-08-21 16:27:42 +0900

    feat: Style Code

--- client/app/src/components/Answer/index.tsx
+++ client/app/src/components/Answer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import * as Styled from "./style.ts"
 
 interface AnswerItemProps {
     author: string;
@@ -9,60 +9,17 @@ interface AnswerItemProps {
 
 const Answer: React.FC<AnswerItemProps> = ({ author, content, time }) => {
     return (
-        <AnswerContainer>
-            <AvatarPlaceholder />
-            <AnswerContent>
-                <AnswerHeader>
-                    <Author>{author}</Author>
-                    <Time>{time} ago</Time>
-                </AnswerHeader>
-                <Content>{content}</Content>
-            </AnswerContent>
-        </AnswerContainer>
+        <Styled.AnswerContainer>
+            <Styled.AvatarPlaceholder />
+            <Styled.AnswerContent>
+                <Styled.AnswerHeader>
+                    <Styled.Author>{author}</Styled.Author>
+                    <Styled.Time>{time} ago</Styled.Time>
+                </Styled.AnswerHeader>
+                <Styled.Content>{content}</Styled.Content>
+            </Styled.AnswerContent>
+        </Styled.AnswerContainer>
     );
 };
 
 export default Answer;
-
-// Styled Components
-const AnswerContainer = styled.div`
-  display: flex;
-  align-items: flex-start;
-  padding: 10px 0;
-  border-bottom: 1px solid #ddd;
-`;
-
-const AvatarPlaceholder = styled.div`
-  width: 40px;
-  height: 40px;
-  background-color: #f0f0f0;
-  border-radius: 50%;
-  margin-right: 10px;
-`;
-
-const AnswerContent = styled.div`
-  flex: 1;
-`;
-
-const AnswerHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 5px;
-`;
-
-const Author = styled.span`
-  font-weight: bold;
-  font-size: 14px;
-`;
-
-const Time = styled.span`
-  font-size: 12px;
-  color: #888;
-`;
-
-const Content = styled.p`
-  font-size: 14px;
-  margin: 0;
-  color: #333;
-`;
--- client/app/src/pages/Question/QuestionDetail/index.tsx
+++ client/app/src/pages/Question/QuestionDetail/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import styled from 'styled-components';
+import * as Style from "./style.ts"
 import Answer from "@/components/Answer";
 import Title from "@/components/Title";
 
@@ -23,90 +23,28 @@ const QuestionDetail: React.FC = () => {
 
     return (<>
         <Title title={"질문 상세보기"}/>
-        <Container>
-            <TitleContainer>
-                <DetailTitle>{item.title}</DetailTitle>
-                <Time>{item.time} ago</Time>
-            </TitleContainer>
-            <Description>{item.description}</Description>
+        <Style.Container>
+            <Style.TitleContainer>
+                <Style.DetailTitle>{item.title}</Style.DetailTitle>
+                <Style.Time>{item.time} ago</Style.Time>
+            </Style.TitleContainer>
+            <Style.Description>{item.description}</Style.Description>
             {item.images && (
-                <ImageContainer>
+                <Style.ImageContainer>
                     {item.images.map((image, index) => (
-                        <Image key={index} src={image} alt={`image-${index}`} />
+                        <Style.Image key={index} src={image} alt={`image-${index}`} />
                     ))}
-                </ImageContainer>
+                </Style.ImageContainer>
             )}
-            <AnswersSection>
-                <SectionTitle>답변</SectionTitle>
+            <Style.AnswersSection>
+                <Style.SectionTitle>답변</Style.SectionTitle>
                 <Answer author="관리자" content="He'll want to use your yacht, and I don't want this thing smelling like fish." time="8m" />
-            </AnswersSection>
-            <CommentInput placeholder="댓글을 입력해주세요" />
-        </Container>
+            </Style.AnswersSection>
+            <Style.CommentInput placeholder="댓글을 입력해주세요" />
+        </Style.Container>
         </>
     );
 };
 
 export default QuestionDetail;
 
-// Styled Components 그대로 유지
-
-// Styled Components
-const Container = styled.div`
-  padding: 20px;
-`;
-
-const TitleContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 10px;
-`;
-
-const DetailTitle = styled.h1`
-  font-size: 20px;
-  font-weight: bold;
-`;
-
-const Time = styled.span`
-  font-size: 12px;
-  color: #888;
-`;
-
-const Description = styled.p`
-  font-size: 14px;
-  margin-bottom: 20px;
-`;
-
-const ImageContainer = styled.div`
-  display: flex;
-  gap: 10px;
-  margin-bottom: 20px;
-`;
-
-const Image = styled.img`
-  width: 100px;
-  height: 100px;
-  object-fit: cover;
-  border-radius: 8px;
-  border: 1px solid #ccc;
-`;
-
-const AnswersSection = styled.div`
-  margin-top: 20px;
-`;
-
-const SectionTitle = styled.h2`
-  font-size: 16px;
-  font-weight: bold;
-  margin-bottom: 10px;
-`;
-
-const CommentInput = styled.input`
-  width: 100%;
-  padding: 10px;
-  font-size: 14px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  margin-top: 10px;
-  box-sizing: border-box;
-`;
