8ca7add0dcb3a57dc43ad51d87db2ed73a656b18
Gu-nuu <gun0005@naver.com>
2024-09-06 14:13:42 +0900

    fix: Login Header의 홈버튼 삭제

--- client/app/src/components/Login/LoginHeader/style.ts
+++ client/app/src/components/Login/LoginHeader/style.ts
@@ -1,16 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    padding-top: 10px;
+    padding-top: 50px;
     width: 100%;
     height: 7vh;
     position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
-`;
-
-export const RightButton = styled.div`
-    position: absolute;
-    right: 40px;
 `;
