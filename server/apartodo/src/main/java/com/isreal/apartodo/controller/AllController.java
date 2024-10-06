8441a0a32eab35469ef6f1191325f306bbad7407
KiminSon <kimin0331@naver.com>
2024-10-07 01:29:04 +0900

    feat: Deny인지 확인하는 기능 구현

--- server/apartodo/src/main/java/com/isreal/apartodo/controller/AllController.java
+++ server/apartodo/src/main/java/com/isreal/apartodo/controller/AllController.java
@@ -26,4 +26,9 @@ public class AllController {
     public List<ApartmentDocument> findApartments(@RequestParam(name = "apartment-name") String apartmentName) {
         return allService.findApartments(apartmentName);
     }
+
+    @GetMapping("/deny/is-deny")
+    public boolean isDeny() {
+        return true;
+    }
 }
