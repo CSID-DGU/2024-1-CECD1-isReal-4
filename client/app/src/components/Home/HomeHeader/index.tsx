eb64076a1a43daaadad9d8878b28e7852fd34501
Changjoon <127469340+CWDll@users.noreply.github.com>
2024-10-16 00:29:27 +0900

    🔥 Remove - Sensitive Code (#33)
    * feat: Instance 생성 + axios + js-cookie
    
    * feat: User Auth and FindApartments API Connection
    
    * feat: Remove Unused Code
    
    * feat: Remove Unused Code
    
    * feat: Remove Unused Code

--- client/app/src/components/Home/HomeHeader/index.tsx
+++ client/app/src/components/Home/HomeHeader/index.tsx
@@ -2,11 +2,24 @@ import * as Styled from "./style.ts"
 import Avatar from "@/assets/icons/Avatar.svg"
 import Alarm from "@/assets/icons/Alarm.svg"
 import SvgButton from "@/components/Common/SvgButton";
+import { checkSuperAdmin } from "@/apis/auth";
+import Cookies from "js-cookie";
+
 
 export default function HomeHeader() {
+
+    const myCookie = Cookies.get("access_token");
+    console.log("Cookie", myCookie);
+
+    const handleCheckSuperAdmin = async () => {
+        const response = await checkSuperAdmin();
+        console.log(response);
+    }
+
+
     return(
         <Styled.Container>
-            <Styled.LeftButton>
+            <Styled.LeftButton onClick={handleCheckSuperAdmin}>
                 <SvgButton src={Avatar} height={"48px"} width={"48px"}/>
             </Styled.LeftButton>
             <Styled.RightButton>
