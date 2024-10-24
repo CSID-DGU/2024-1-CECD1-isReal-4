11f682bfa31a48c17e802fa27c9682b0a8a1ad3f
Gu-nuu <gun0005@naver.com>
2024-10-25 01:59:02 +0900

    feat: Admin Login Router Setting

--- client/web/src/Router.tsx
+++ client/web/src/Router.tsx
@@ -1,10 +1,12 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Auth from "@/pages/Auth";
+import Login from "./pages/Login";
 
 export default function Router() {
     return (
         <Routes>
-            <Route path="/" element={<Auth/>}/>
+            <Route path='/' element={<Auth />} />
+            <Route path='/login' element={<Login />} />
         </Routes>
-    )
+    );
 }
